c311db3d6365962afb92d16fa9e00ac88045964f
Ravi Anand <ravi.anand@nbt.ag>
2019-07-13 01:25:19 +0200

    Install and configure `vuelidate`.
    -test vuelidate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,6 +18,32 @@
           Welcome to Vuetify <br/>
           Translation Test: {{$t('hello')}}
         </h1>
+        <v-flex>
+          <h2>
+            Vuelidate test:
+          </h2>
+          <div>
+            <div class="form-group" :class="{ 'form-group--error': $v.name.$error }">
+              <label class="form__label">Name</label>
+              <input class="form__input" v-model.trim="$v.name.$model"/>
+            </div>
+            <div class="error" v-if="!$v.name.required">Field is required</div>
+            <div class="error" v-if="!$v.name.minLength">Name must have at least {{$v.name.$params.minLength.min}}
+              letters.
+            </div>
+            <tree-view :data="$v.name" :options="{rootObjectKey: '$v.name', maxDepth: 2}"></tree-view>
+            <div class="form-group" :class="{ 'form-group--error': $v.age.$error }">
+              <label class="form__label">Age</label>
+              <input class="form__input" v-model.trim.lazy="$v.age.$model"/>
+            </div>
+            <div class="error" v-if="!$v.age.between">Must be between {{$v.age.$params.between.min}} and
+              {{$v.age.$params.between.max}}
+            </div>
+            <span tabindex="0">Blur to see changes</span>
+            <tree-view :data="$v.age" :options="{rootObjectKey: '$v.age', maxDepth: 2}"></tree-view>
+          </div>
+        </v-flex>
+
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br>please join our online
@@ -86,60 +112,77 @@
 </template>
 
 <script>
-export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
-      }
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuetifyjs.com'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
+import { required, minLength, between } from 'vuelidate/lib/validators'
 
-    ]
-  })
+export default {
+  data () {
+    return {
+      name: '',
+      age: 0,
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader'
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify'
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify'
+        }
+      ],
+      importantLinks:
+        [
+          {
+            text: 'Documentation',
+            href: 'https://vuetifyjs.com'
+          },
+          {
+            text: 'Chat',
+            href: 'https://community.vuetifyjs.com'
+          },
+          {
+            text: 'Made with Vuetify',
+            href: 'https://madewithvuetifyjs.com'
+          },
+          {
+            text: 'Twitter',
+            href: 'https://twitter.com/vuetifyjs'
+          },
+          {
+            text: 'Articles',
+            href: 'https://medium.com/vuetify'
+          }
+        ],
+      whatsNext:
+        [
+          {
+            text: 'Explore components',
+            href: 'https://vuetifyjs.com/components/api-explorer'
+          },
+          {
+            text: 'Select a layout',
+            href: 'https://vuetifyjs.com/layout/pre-defined'
+          },
+          {
+            text: 'Frequently Asked Questions',
+            href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+          }
+
+        ]
+    }
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    age: {
+      between: between(20, 30)
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import router from './router'
 import store from './store'
 import i18n from './plugins/i18n'
 
+import Vuelidate from 'vuelidate'
+
 // styles
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 
 new Vue({
   i18n,
