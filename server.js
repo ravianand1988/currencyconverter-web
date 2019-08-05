47b93b4292add06a055171d9ccaf367cf95ca593
Ravi Anand <ravi.anand@nbt.ag>
2019-08-05 21:33:26 +0200

    Update port to 8080

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require('serve-static')
 const path = require('path')
 let app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`listening on port: ${port}`)
