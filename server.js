c17dd9a7d21a6572bfdbb7dd1fb1349cf16968cc
Ravi Anand <ravi.anand@nbt.ag>
2019-08-05 16:56:39 +0200

    Add /dist on git to serve on heroku.
    updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 let app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 80
 app.listen(port)
+console.log(`listening on port: ${port}`)
