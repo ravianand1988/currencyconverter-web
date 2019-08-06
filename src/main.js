30f5e029a75331f34234a4312f1e3be68a301645
Ravi Anand <ravi.anand@nbt.ag>
2019-08-06 11:38:02 +0200

    Remove all dependencies from main.js
    - heroku is not able to serve this app.

--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
 import Vue from 'vue'
-import './plugins/vuetify'
+// import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import i18n from './plugins/i18n'
+// import i18n from './plugins/i18n'
 
-import Vuelidate from 'vuelidate'
-import Meta from 'vue-meta'
-import FlagIcon from 'vue-flag-icon'
-import moment from 'moment'
+// import Vuelidate from 'vuelidate'
+// import Meta from 'vue-meta'
+// import FlagIcon from 'vue-flag-icon'
+// import moment from 'moment'
 
 // styles
-import 'vuetify/dist/vuetify.min.css'
+// import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuelidate)
-Vue.use(Meta, {
-  refreshOnceOnNavigation: true
-})
-Vue.use(FlagIcon)
+// Vue.use(Vuelidate)
+// Vue.use(Meta, {
+//   refreshOnceOnNavigation: true
+// })
+// Vue.use(FlagIcon)
 
 Vue.config.productionTip = false
-moment.locale('en')
-Vue.prototype.moment = moment
+// moment.locale('en')
+// Vue.prototype.moment = moment
 
 new Vue({
-  i18n,
   router,
   store,
   render: h => h(App)
